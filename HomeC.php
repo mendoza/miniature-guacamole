f20261792e80972d5ff0f7d736c032f8e9cde3bc
carlosvhz <carlosvhz@outlook.com>
2018-09-16 14:51:57 -0600

    RealizarPrueba

---
+++ HomeC.php
@@ -0,0 +1,28 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="./HomeC.php" method="post">
+      <input type="number" min="1" max="100" name="edad" value="1">
+      <input type="submit" name="JOH" value="submit">
+    </form>
+    <?php+
+      for ($i=0; $i < 2; $i++) {
+        echo("<h1>Wenas</h1>"); #imprimir en el hmtl
+        print_r("asdasdasdsadawdasdas"); #imprimir exactamente
+      }
+      $dbconnect =  pg_connect("host=159.89.34.186 dbname=prueba user=postgres password=papitopiernaslargas69");
+      if($dbconnect){
+
+        $table =  print_r(pg_fetch_all(pg_query($dbconnect,"SELECT * FROM PIPOL")));
+        foreach ($table as $p) {
+          $s = $p['id'];
+          echo "<li>$s</li>";
+        }
+      }
+    ?>
+  </body>
+</html>
---
+++ RealizarPrueba.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Realizar Prueba</title>
+  </head>
+  <body>
+    <h1>Realizar Prueba</h1>
+    <p>Ingrese nombre o codigo del tecnico</p>
+    <p>Hola:3 </p>
+    <form action="/action_page.php">
+      Codigo o nombre: <input type="text" name="fname"><br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
